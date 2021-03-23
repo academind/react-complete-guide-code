3bcad072ea2fbd88fa6983344aac7c68d656c118
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-03-23 17:36:02 +0100

    added code

---
+++ code/07-adding-normal-javascript-logic/src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className='expense-item'>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

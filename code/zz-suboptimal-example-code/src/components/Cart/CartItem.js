8d1a4ab6d21389bc9b140d48c65166146034d380
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-03-24 13:56:12 +0100

    added missing attachments

---
+++ code/zz-suboptimal-example-code/src/components/Cart/CartItem.js
@@ -0,0 +1,47 @@
+import { useDispatch } from 'react-redux';
+
+import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cart-slice';
+
+const CartItem = (props) => {
+  const dispatch = useDispatch();
+
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+  return (
+    <li className={classes.item}>
+      <header>
+        <h3>{title}</h3>
+        <div className={classes.price}>
+          ${total.toFixed(2)}{' '}
+          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+        </div>
+      </header>
+      <div className={classes.details}>
+        <div className={classes.quantity}>
+          x <span>{quantity}</span>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
---
+++ extra-files/Notification.js
@@ -0,0 +1,23 @@
+import classes from './Notification.module.css';
+
+const Notification = (props) => {
+  let specialClasses = '';
+
+  if (props.status === 'error') {
+    specialClasses = classes.error;
+  }
+  if (props.status === 'success') {
+    specialClasses = classes.success;
+  }
+
+  const cssClasses = `${classes.notification} ${specialClasses}`;
+
+  return (
+    <section className={cssClasses}>
+      <h2>{props.title}</h2>
+      <p>{props.message}</p>
+    </section>
+  );
+};
+
+export default Notification;

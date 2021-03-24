316f502f127b4fb613b2a0099a4b90256e96b1c1
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-03-24 08:17:01 +0100

    added code and slides

---
+++ code/02-react-fragments/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, Fragment } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </Fragment>
+  );
+}
+
+export default App;

158a8159757c5fb15d825f6d28b48fb4ee2ff4cf
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-03-24 08:52:14 +0100

    updated code

--- code/01-starting-project/src/components/User.js
+++ code/01-starting-project/src/components/User.js
@@ -1,15 +1,7 @@
-import { Component } from 'react';
-
 import classes from './User.module.css';
 
-class User extends Component {
-  render() {
-    return <li className={classes.user}>{this.props.name}</li>;
-  } 
-}
-
-// const User = (props) => {
-//   return <li className={classes.user}>{props.name}</li>;
-// };
+const User = (props) => {
+  return <li className={classes.user}>{props.name}</li>;
+};
 
 export default User;

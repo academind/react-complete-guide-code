3a24779a0caf4b8f20aada240d17293685e43fac
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-04-27 15:17:01 +0200

    fixed mealitem ids

--- code/11-making-items-removable/src/components/Meals/AvailableMeals.js
+++ code/11-making-items-removable/src/components/Meals/AvailableMeals.js
@@ -32,8 +32,8 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
-      
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}

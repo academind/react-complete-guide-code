992e7c980edd25ab25be03088ca1aa25bad8a53b
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2021-08-30 16:36:14 +0200

    assignment 1

--- code/08-splitting-components-into-multiple-components/src/App.js
+++ code/08-splitting-components-into-multiple-components/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -26,25 +26,8 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
+      <Expenses
+        expenses={expenses}
       />
     </div>
   );

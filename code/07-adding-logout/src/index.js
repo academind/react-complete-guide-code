bdd8edee4f2bb3ab8d7e3f7543445c2fb871b74e
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-12-17 16:58:33 +0100

    updated to React 18

--- code/07-adding-logout/src/index.js
+++ code/07-adding-logout/src/index.js
@@ -5,11 +5,11 @@ import './index.css';
 import App from './App';
 import { AuthContextProvider } from './store/auth-context';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <AuthContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </AuthContextProvider>,
-  document.getElementById('root')
+  </AuthContextProvider>
 );

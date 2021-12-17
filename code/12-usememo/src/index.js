fa52d75605dd96b6e8624394679c1077a183f51e
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-12-17 17:06:47 +0100

    updated to React 18

--- code/12-usememo/src/index.js
+++ code/12-usememo/src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 import App from './App';
 import AuthContextProvider from './context/auth-context';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <AuthContextProvider>
     <App />
-  </AuthContextProvider>,
-  document.getElementById('root')
+  </AuthContextProvider>
 );

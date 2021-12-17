a8899285f89a5fc78fb593e4f686bfbd644fde82
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-12-17 17:05:04 +0100

    updated to React 18

--- code/07-react-ts-starting-project/src/index.tsx
+++ code/07-react-ts-starting-project/src/index.tsx
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

c5b43babbbb09675de9843da302eae9c4171091a
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-12-17 16:59:41 +0100

    updated to React 18

--- code/01-starting-project/src/index.js
+++ code/01-starting-project/src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 registerServiceWorker();

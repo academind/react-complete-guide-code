5a2d6decba4c06061c5da85fd253d216f1122d2d
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-12-17 16:54:09 +0100

    updated to React 18

--- code/11-reading-and-dispatching/src/index.js
+++ code/11-reading-and-dispatching/src/index.js
@@ -6,9 +6,9 @@ import './index.css';
 import App from './App';
 import store from './store/index';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );

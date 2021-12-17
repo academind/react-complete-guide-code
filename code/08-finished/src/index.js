eb25b4cf287ca4d4a8b6bdc65aace57820bfbb2c
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-12-17 16:41:58 +0100

    updated to React 18

--- code/08-finished/src/index.js
+++ code/08-finished/src/index.js
@@ -4,4 +4,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);

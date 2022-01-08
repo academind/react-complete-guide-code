16443b4666eaf83948045f21f0b95f7b2e59e210
davidchen003 <89047538+davidchen003@users.noreply.github.com>
2022-01-08 13:31:46 -0800

    Update NewMeetup.js
    In react-router-dom v6 useHistory() is replaced by useNavigate().

--- code/17-navigating-programmatically/src/pages/NewMeetup.js
+++ code/17-navigating-programmatically/src/pages/NewMeetup.js
@@ -1,22 +1,18 @@
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  const history = useHistory();
-
+  const navigate = useNavigate();
   function addMeetupHandler(meetupData) {
-    fetch(
-      'https://react-getting-started-48dec-default-rtdb.firebaseio.com/meetups.json',
-      {
-        method: 'POST',
-        body: JSON.stringify(meetupData),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    ).then(() => {
-      history.replace('/');
+    fetch("https://academind2-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      navigate("/");
     });
   }
 

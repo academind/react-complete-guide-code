65b22802b9572bbaa5dea2bd94df1a36ce5e8bc3
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2022-03-25 17:47:17 +0100

    updated code attachments to v18

--- code/04-adding-individual-meal-items/src/index.js
+++ code/04-adding-individual-meal-items/src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
--- code/07-adding-a-modal/src/components/UI/Modal.js
+++ code/07-adding-a-modal/src/components/UI/Modal.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import classes from './Modal.module.css';
 

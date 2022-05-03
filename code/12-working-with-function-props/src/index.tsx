935039cf904828ce3705aea2256cab71f92ee7e0
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2022-05-03 13:38:06 +0200

    updated react 18 dependencies

--- code/12-working-with-function-props/src/index.tsx
+++ code/12-working-with-function-props/src/index.tsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);
 

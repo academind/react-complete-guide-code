650aeec0870ccff25b522d5ed2090fbcd4ee3908
MrLeo7 <106233079+MrLeo7@users.noreply.github.com>
2022-05-25 12:15:06 +0400

    Missing Fetch 'POST' method.
    Updated according to tutorioal.

--- code/06-preparing-the-project-for-the-next-steps/src/App.js
+++ code/06-preparing-the-project-for-the-next-steps/src/App.js
@@ -13,22 +13,26 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-7efc4-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
+      const loadedMovies = [];
+
+      for (const key in data){
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
       });
-      setMovies(transformedMovies);
+      }
+
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +43,15 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response =  await fetch('https://react-http-7efc4-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: 
+      {'Content-Type': 'application/json'}
+
+    });
+    
   }
 
   let content = <p>Found no movies.</p>;

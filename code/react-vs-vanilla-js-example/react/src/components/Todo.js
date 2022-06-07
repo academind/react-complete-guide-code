1391f4be881e4700dd1945796b4eff87380e9351
Aymen Jelassi <33674133+aymen-jelassi@users.noreply.github.com>
2022-06-07 13:46:11 +0000

    Bro

--- code/react-vs-vanilla-js-example/react/src/components/Todo.js
+++ code/react-vs-vanilla-js-example/react/src/components/Todo.js
@@ -4,28 +4,30 @@ import Backdrop from './Backdrop';
 import Modal from './Modal';
 
 function Todo(props) {
-  const [showModal, setShowModal] = useState();
+	const [showModal, setShowModal] = useState();
 
-  function showModalHandler() {
-    setShowModal(true);
-  }
+	function showModalHandler() {
+		setShowModal(true);
+	}
 
-  function closeModalHandler() {
-    setShowModal(false);
-  }
+	function closeModalHandler() {
+		setShowModal(false);
+	}
 
-  return (
-    <div className='card'>
-      <h2>{props.text}</h2>
-      <div className='actions'>
-        <button className='btn' onClick={showModalHandler}>
-          Delete
-        </button>
-      </div>
-      {showModal && <Backdrop onClick={closeModalHandler} />}
-      {showModal && <Modal text='Are you sure?' onClose={closeModalHandler} />}
-    </div>
-  );
+	return (
+		<div className="card">
+			<h2>{props.text}</h2>
+			<div className="actions">
+				<button className="btn" onClick={showModalHandler}>
+					Delete
+				</button>
+			</div>
+			{showModal && <Backdrop onClick={closeModalHandler} />}
+			{showModal && (
+				<Modal text="Are you sure? My Brother" onClose={closeModalHandler} />
+			)}
+		</div>
+	);
 }
 
 export default Todo;

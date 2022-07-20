d18dd5671e34a65f081b322d2f35286653b232a3
elvary <elvis.van@outlook.com>
2022-07-20 18:29:59 +0700

    add packages & git ignore

--- code/01-working-with-state/src/App.js
+++ code/01-working-with-state/src/App.js
@@ -25,13 +25,6 @@ const App = () => {
     },
   ];
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
-
   return (
     <div>
       <h2>Let's get started!</h2>

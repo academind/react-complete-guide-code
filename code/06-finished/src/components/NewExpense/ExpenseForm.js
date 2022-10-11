89de3f295717633b7361c8f2dfdf8fbc1b5f04d6
Istiaque Ahmed Nuhash <nuhash18@gmail.com>
2022-10-12 01:01:57 +0600

    Update ExpenseForm.js
    fixing adding value for multiple expenses

--- code/06-finished/src/components/NewExpense/ExpenseForm.js
+++ code/06-finished/src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
